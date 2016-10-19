8bcd300a08b758aad658ebb7d8ac067c989098a8
Sravana Reddy <sravana@cs.dartmouth.edu>
2016-10-19 00:23:07 -0400

    minor: change consolidate to work for multiple subdirectories

--- consolidate_summaries.py
+++ consolidate_summaries.py
@@ -1,16 +1,19 @@
 import ujson, os
 
-DATADIR = 'rawdata/00'
+DATADIR = 'rawdata/'
 
 # load summaries
 summaries = {}
-for userid in os.listdir(DATADIR):
-    summaries[userid] = {}
-    for projectname in os.listdir(os.path.join(DATADIR, userid)):
-        if projectname.endswith('_summary.json'):
-            summaries[userid][projectname.rsplit('_', 1)[0]] = ujson.load(open(os.path.join(DATADIR,
-                                                                                           userid,
-                                                                                           projectname)))
+for subdir in os.listdir(DATADIR):
+    print subdir
+    for userid in os.listdir(os.path.join(DATADIR, subdir)):
+        summaries[userid] = {}
+        for projectname in os.listdir(os.path.join(DATADIR, subdir, userid)):
+            if projectname.endswith('_summary.json'):
+                summaries[userid][projectname.rsplit('_', 1)[0]] = ujson.load(open(os.path.join(DATADIR,
+                                                                                                subdir,
+                                                                                                userid,
+                                                                                                projectname)))
 
 
 print 'Loaded summaries for', len(summaries), 'users'
@@ -18,5 +21,3 @@ print sum(map(len, summaries.values())), 'projects'
 
 with open('user_project_summaries.json', 'w') as o:
     ujson.dump(summaries, o, indent=2)
-
-
