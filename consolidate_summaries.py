00daf363b22372b354a9e8ec5612992cec624831
Sravana Reddy <sravana@cs.dartmouth.edu>
2016-10-19 11:53:25 -0400

    minor: put each group of 1000 in a line

--- consolidate_summaries.py
+++ consolidate_summaries.py
@@ -3,21 +3,19 @@ import ujson, os
 DATADIR = 'rawdata/'
 
 # load summaries
-summaries = {}
-for subdir in os.listdir(DATADIR):
-    print subdir
-    for userid in os.listdir(os.path.join(DATADIR, subdir)):
-        summaries[userid] = {}
-        for projectname in os.listdir(os.path.join(DATADIR, subdir, userid)):
-            if projectname.endswith('_summary.json'):
-                summaries[userid][projectname.rsplit('_', 1)[0]] = ujson.load(open(os.path.join(DATADIR,
+with open('user_project_summaries.json', 'w') as o:
+    for subdir in os.listdir(DATADIR):
+        summaries = {}
+        for i, userid in enumerate(os.listdir(os.path.join(DATADIR, subdir))):
+            if (i+1)%50==0:
+                print '.',
+            summaries[userid] = {}
+            for projectname in os.listdir(os.path.join(DATADIR, subdir, userid)):
+                if projectname.endswith('_summary.json'):
+                    summaries[userid][projectname.rsplit('_', 1)[0]] = ujson.load(open(os.path.join(DATADIR,
                                                                                                 subdir,
                                                                                                 userid,
                                                                                                 projectname)))
-
-
-print 'Loaded summaries for', len(summaries), 'users'
-print sum(map(len, summaries.values())), 'projects'
-
-with open('user_project_summaries.json', 'w') as o:
-    ujson.dump(summaries, o, indent=2)
+        o.write(ujson.dumps(summaries))
+        o.write('\n')
+        print subdir
