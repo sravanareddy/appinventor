3f244aa635cf7b62831c174b55d1b315d5a0ef1e
Sravana Reddy <sravana@cs.dartmouth.edu>
2016-11-23 00:22:41 -0500

    code cleanup and some fixes

--- features.py
+++ features.py
@@ -1,86 +1,69 @@
 #features.py
-import datetime 
-import time 
+import datetime
+import numpy
 
-def getAverageProjectLen(projects):
-    num_projects = len(projects)
-    total_project_length = 0 #the sum of all the lengths of the project (from date created to date modified)
-    
+##TIME FEATURES
+def timediff(t1, t2):
+    """difference between times (millisec precision) as days"""
+    return (t1-t2)/(86400.*1000)
+
+def getProjectIntervals(projects):
+    """List of intervals between consecutive projects"""
+    intervals = []
     prev = projects[0]["**created"]
-    for project in projects[1:]: 
+    for project in projects[1:]:
         curr = project['**created']
-        total_project_length += (curr - prev)/(86400.*1000)   #SR: added this to scale to days
-        prev = curr 
-    return int(total_project_length / float(num_projects))
+        intervals.append(timediff(curr, prev))
+        prev = curr
+    return numpy.array(intervals)
+
+def getProjectLengths(projects):
+    """Lengths (according to last modified date) of projects"""
+    return numpy.array([timediff(project["**modified"], project["**created"]) for project in projects])
 
 def numProjects(projects):
 	return len(projects)
 
-def percentOnWeekday(projects):
-    num_projs = numProjects(projects)
-    weekday_projs = 0
-    for project in projects: 
-        if datetime.datetime.fromtimestamp(project['**created'] / 1e3).weekday() < 6: 
-            weekday_projs += 1.0
-    return weekday_projs / num_projs * 100
-
-def percentOnDay(projects):
+def numOnDay(projects):
+    """number of projects per day of the week"""
     num_projs = len(projects)
     weekday_projs = 0
-    dow = [] 
+    dow = []
     for i in range(7):
         dow.append(0)
-    for project in projects: 
-        date = datetime.datetime.fromtimestamp(project['**created'] / 1e3).weekday() 
+    for project in projects:
+        date = datetime.datetime.fromtimestamp(project['**created'] / 1e3).weekday()
         dow[date] += 1.0
-    for i in range(7):
-        dow[i] = dow[i] / num_projs * 100 
-    return dow
-    
-def numProjectsInDecile(projects):
-    ''' the first decile is the 0th decile (covering from 0-10% of the time), 
-    the last decile is the 9th decile'''
-    first= int(projects[0]['**created'])
-    last = int(projects[len(projects)-1]['**created'])
-    decile_width = ((last - first) / (86400 * 1000.)) / 10
+    return numpy.array(dow)
 
-    decileList = []   
-    for i in range(10): 
-        decileList.append(0)
-        
-    for project in projects: 
-        daysElapsed = (project['**created']-first)/(86400 * 1000.)
-        if daysElapsed == 0: 
-            index = 0
-        else: 
-            index  = int(daysElapsed / decile_width)
-        if index == 10: 
-            decileList[9] = decileList[9] + 1
-        else: 
-            decileList[index] = decileList[index] + 1
-    return decileList
+def percentOnWeekday(projectsdow):
+    """percentage of projects on a weekday, given number of projects for each day"""
+    return sum(projectsdow[:5]) / sum(projectsdow) * 100
 
-##CODE FEATURES 
+def projectsPerUserPeriod(projects, bins=10):
+    '''histogram of number of projects across user's lifespan'''
+    first= projects[0]['**created']
+    daysElapsed = [timediff(project['**created'], first) for project in projects]
+    hist, _ = numpy.histogram(daysElapsed, bins=bins)
+    return hist
+
+##CODE FEATURES
 def getNumScreens(projects):
     numScreens = []
-    for project in projects: 
+    for project in projects:
          numScreens.append(project['*Number of Screens'])
-    return numScreens
+    return numpy.array(numScreens)
 
-def averageNumScreens(projects):
-    screenList = getNumScreens(projects)
-    return sum(screenList)/ len(screenList)  
-
-def getAllVariables(projects): 
+def getAllVariables(projects):
     '''returns a list with 0th element being the average number of local variables, the 1st element
     being the average number of global variables, and the 2nd element being the average number
     of variables in a project'''
     numScreens = getNumScreens(projects)
-    gv_count = 0 
-    lv_count = 0 
-    i = 0 
+    gv_count = 0
+    lv_count = 0
+    i = 0
     variables = []
-    while i < len(projects): 
+    while i < len(projects):
         for j in range(numScreens[i]):
             screenNum = "Screen"+ str(j + 1)
             if screenNum in projects[i].keys():
@@ -92,14 +75,14 @@ def getAllVariables(projects):
     variables.append(lv_count)
     variables.append(gv_count)
     variables.append(lv_count + gv_count)
-    
+
     return [v/float(len(projects)) for v in variables]
 
 def averageNumBlocks(projects):
     numScreens = getNumScreens(projects)
-    totalBlocks = 0 
-    i = 0 
-    while i < len(projects): 
+    totalBlocks = 0
+    i = 0
+    while i < len(projects):
         for j in range(numScreens[i]):
             screenNum = "Screen"+ str(j + 1)
             if screenNum in projects[i].keys():
@@ -111,9 +94,9 @@ def averageNumBlocks(projects):
 
 def getAverageTypeTLBlocks(projects):
     numScreens = getNumScreens(projects)
-    tl_count = 0 
-    i = 0 
-    while i < len(projects): 
+    tl_count = 0
+    i = 0
+    while i < len(projects):
         for j in range(numScreens[i]):
             screenNum = "Screen"+ str(j + 1)
             if screenNum in projects[i].keys():
@@ -124,19 +107,19 @@ def getAverageTypeTLBlocks(projects):
 
 def getAverageOrphanBlocks(projects):
     numScreens = getNumScreens(projects)
-    tl_count = 0 
-    i = 0 
-    while i < len(projects): 
+    tl_count = 0
+    i = 0
+    while i < len(projects):
         for j in range(numScreens[i]):
             screenNum = "Screen"+ str(j + 1)
             if screenNum in projects[i].keys():
                 if  not isinstance(projects[i][screenNum]['Blocks'], unicode):
-                    if projects[i][screenNum]['Blocks']['Orphan Blocks'] != "NO ORPHAN BLOCKS": 
+                    if projects[i][screenNum]['Blocks']['Orphan Blocks'] != "NO ORPHAN BLOCKS":
                         tl_count += projects[i][screenNum]['Blocks']['Orphan Blocks']['*Number of Blocks']
         i+=1
     return tl_count / float(len(projects))
 
-def decileNumScreens(projects): 
+def decileNumScreens(projects):
     all_deciles = []
     for i in range(10):
         all_deciles.append([])
@@ -145,66 +128,66 @@ def decileNumScreens(projects):
     last = int(projects[len(projects)-1]['**created'])
     decile_width = ((last - first) / (86400 * 1000.)) / 10
 
-    for project in projects: 
+    for project in projects:
         numScreens = project['*Number of Screens']
         daysElapsed = (project['**created']-first)/(86400 * 1000.)
-        #set index 
-        if daysElapsed == 0: 
+        #set index
+        if daysElapsed == 0:
             index = 0
-        else: 
+        else:
             index  = int(daysElapsed / decile_width)
 
-        if index == 10: 
+        if index == 10:
             all_deciles[9].insert(0,numScreens)
-        else: 
+        else:
             all_deciles[index].insert(0,numScreens)
-        
+
     for i in range(10):
-        if len(all_deciles[i]) == 0: 
+        if len(all_deciles[i]) == 0:
             all_deciles[i] = 0
-        else: 
+        else:
             all_deciles[i] = sum(all_deciles[i])/ float(len(all_deciles[i]))
     return all_deciles
 
-def decileTypesTopLevelBlocks(projects): 
+def decileTypesTopLevelBlocks(projects):
     '''measures how many types of TLblocks per project in each decile '''
     all_deciles = []
     for i in range(10):
         all_deciles.append([])
-        
+
     first= int(projects[0]['**created'])
     last = int(projects[len(projects)-1]['**created'])
     decile_width = ((last - first) / (86400 * 1000.)) / 10
 
 
-    for project in projects: 
+    for project in projects:
         numTL = 0
         numScreens = project['*Number of Screens']
         daysElapsed = (project['**created']-first)/(86400 * 1000.)
-        
+
         for j in range(numScreens):
             screenNum = "Screen"+ str(j + 1)
             if screenNum in project.keys():
                 if  not isinstance(project[screenNum]['Blocks'], unicode):
                     numTL = len(project[screenNum]['Blocks']['*Top Level Blocks'].keys())
-    #set index 
-                if daysElapsed == 0: 
+    #set index
+                if daysElapsed == 0:
                     index = 0
-                else: 
+                else:
                     index  = int(daysElapsed / decile_width)
-    
-                if index == 10: 
+
+                if index == 10:
                     all_deciles[9].insert(0,numTL)
-                else: 
+                else:
                     all_deciles[index].insert(0,numTL)
     for i in range(10):
-        if len(all_deciles[i]) == 0: 
+        if len(all_deciles[i]) == 0:
             all_deciles[i] = 0
-        else: 
+        else:
             all_deciles[i] = sum(all_deciles[i])/ float(len(all_deciles[i]))
     return all_deciles
 
-def decileOrphanBlocks(projects): 
+def decileOrphanBlocks(projects):
     '''measures how many orphan blocks per project in each decile '''
     all_deciles = []
     for i in range(10):
@@ -212,9 +195,9 @@ def decileOrphanBlocks(projects):
     first= int(projects[0]['**created'])
     last = int(projects[len(projects)-1]['**created'])
     decile_width = ((last - first) / (86400 * 1000.)) / 10
-        
-    for project in projects: 
-        numOrphan =0 
+
+    for project in projects:
+        numOrphan =0
         numScreens = project['*Number of Screens']
         daysElapsed = (project['**created']-first)/(86400 * 1000.)
         for j in range(numScreens):
@@ -224,55 +207,55 @@ def decileOrphanBlocks(projects):
                     if project[screenNum]['Blocks']['Orphan Blocks'] != "NO ORPHAN BLOCKS":
                         numOrphan += len(project[screenNum]['Blocks']['Orphan Blocks'].keys())
 
-                    if daysElapsed == 0: 
+                    if daysElapsed == 0:
                         index = 0
-                    else: 
+                    else:
                         index  = int(daysElapsed / decile_width)
-        
-                    if index == 10: 
+
+                    if index == 10:
                         all_deciles[9].insert(0,numOrphan)
-                    else: 
+                    else:
                         all_deciles[index].insert(0,numOrphan)
-      
+
     for i in range(10):
-        if len(all_deciles[i]) == 0: 
+        if len(all_deciles[i]) == 0:
             all_deciles[i] = 0
-        else: 
+        else:
             all_deciles[i] = sum(all_deciles[i])/ float(len(all_deciles[i]))
     return all_deciles
-    
+
 def getAverageNumTLBlocks(projects):
     numScreens = getNumScreens(projects)
-    count = 0 
-    i = 0 
-    while i < len(projects): 
+    count = 0
+    i = 0
+    while i < len(projects):
         for j in range(numScreens[i]):
             screenNum = "Screen"+ str(j + 1)
             if screenNum in projects[i].keys():
                 if  not isinstance(projects[i][screenNum]['Blocks'], unicode):
                     types = projects[i][screenNum]['Blocks']['*Top Level Blocks'].keys()
-                    for block in types: 
+                    for block in types:
                         count += projects[i][screenNum]['Blocks']['*Top Level Blocks'][block]
         i+=1
     return count / float(len(projects))
 
 def averageNumComponents(projects):
     numScreens = getNumScreens(projects)
-    numC = 0 
-    i = 0 
-    while i < len(projects): 
+    numC = 0
+    i = 0
+    while i < len(projects):
         for j in range(numScreens[i]):
             screenNum = "Screen"+ str(j + 1)
             if screenNum in projects[i].keys():
-                if  not isinstance(projects[i][screenNum]['Components'], unicode):           
+                if  not isinstance(projects[i][screenNum]['Components'], unicode):
 #                    if 'Number of Components' in projects[i][screenNum]['Components'].keys():
                     if not isinstance(projects[i][screenNum]['Components']['Number of Components'],unicode):
                         numC += projects[i][screenNum]['Components']['Number of Components']
         i+=1
     return numC / float(len(projects))
 
-def aveNumMediaAssets(projects): 
-    count = 0 
-    for project in projects: 
+def aveNumMediaAssets(projects):
+    count = 0
+    for project in projects:
         count += len(project['*Media Assets'])
     return count / float(len(projects))
---
+++ plotcnf.py
@@ -0,0 +1,25 @@
+"""Code from http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py to visualize confusion matrix"""
+
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(cm, classes, normalize=False, cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title('Confusion Matrix')
+    plt.colorbar()
+    tick_marks = range(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
