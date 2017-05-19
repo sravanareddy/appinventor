bf5a1dccff2a45a5746b7bff25d102b4fc3f63ea
Sravana Reddy <sravana@cs.dartmouth.edu>
2017-05-19 07:07:32 -0400

    simplified blocks functions

--- features.py
+++ features.py
@@ -48,9 +48,9 @@ def projectsPerUserPeriod(projects, bins=10):
     return hist
 
 ##CODE FEATURES
-def getScreenNames(projects): 
+def getScreenNames(projects):
     screenNames = []
-    for project in projects: 
+    for project in projects:
         projectScreens = [key for key in project.keys() if '*' not in key]
         screenNames.append(projectScreens)
     return screenNames
@@ -173,7 +173,7 @@ def averageNumStrings(projects): #counts unique strings
         i+=1
     return s / float(len(projects))
 
-def averageNumTypeComponents(projects): #ignores duplicate components, tests variety 
+def averageNumTypeComponents(projects): #ignores duplicate components, tests variety
     numScreens = getScreenNames(projects)
     numC = 0
     i = 0
@@ -187,7 +187,7 @@ def averageNumTypeComponents(projects): #ignores duplicate components, tests var
         i+=1
     return numC / float(len(projects))
 
-def averageNumProcedures(projects): 
+def averageNumProcedures(projects):
     numScreens = getScreenNames(projects)
     numProc = 0
     i = 0
@@ -204,7 +204,7 @@ def averageNumProcedures(projects):
 def deltaDeciles(decileDict, name):
     values = sortDeciles(decileDict)
     deltas = {}
-    for i in range(10): 
+    for i in range(10):
         if i== 0: pass
         else: deltas[str(i) + " " + name] = values[i]-values[i-1]
     return deltas
@@ -213,19 +213,19 @@ def deltaDeciles(decileDict, name):
 def sortDeciles(decileDict):
     deciles_sorted = []
     keys = decileDict.keys()
-    for key in keys: 
+    for key in keys:
         deciles_sorted.insert(int(key[-1]), decileDict[key])
     return deciles_sorted
 
 
-def normalizeDict(d, projects): 
+def normalizeDict(d, projects):
     '''normalizes dictionary by # of projects'''
     np = numProjects(projects)
     if float(np) == 0: np == 1
-    for entry in d: 
-        try: 
+    for entry in d:
+        try:
             d[entry] = d[entry] / float(np)
-        except ZeroDivisionError: 
+        except ZeroDivisionError:
             print entry, np
     return d
 
@@ -238,16 +238,16 @@ def getControlsBlocks(projects):
         for screenName in numScreens[i]:
             if screenName in projects[i] and 'Active Blocks' in projects[i][screenName]['Blocks'] and 'Types' in projects[i][screenName]['Blocks']['Active Blocks']:
                 allBlocks = projects[i][screenName]['Blocks']['Active Blocks']['Types']
-                
-                if 'controls_if' in allBlocks: 
+
+                if 'controls_if' in allBlocks:
                     controls_dict['controls_if'] += projects[i][screenName]['Blocks']['Active Blocks']['Types']['controls_if']
-                if 'controls_forEach' in allBlocks: 
+                if 'controls_forEach' in allBlocks:
                     controls_dict['controls_forEach'] += projects[i][screenName]['Blocks']['Active Blocks']['Types']['controls_forEach']
-                if 'controls_while' in allBlocks: 
+                if 'controls_while' in allBlocks:
                     controls_dict['controls_while'] += projects[i][screenName]['Blocks']['Active Blocks']['Types']['controls_while']
-                if 'controls_choose' in allBlocks: 
+                if 'controls_choose' in allBlocks:
                     controls_dict['controls_choose'] += projects[i][screenName]['Blocks']['Active Blocks']['Types']['controls_choose']
-               
+
         i+=1
     final_dict = normalizeDict(controls_dict, projects)
     return final_dict
@@ -258,86 +258,79 @@ def getControlsBlocks(projects):
 def getClasses(projects):
     class_dict = {u'TableArrangement':0, u'DatePicker':0, u'Canvas':0,
                   u'CheckBox':0, u'Web':0, u'Clock':0, u'BluetoothServer':0, u'ActivityStarter':0,
-                  u'Texting':0, u'Label':0, u'Spinner':0, u'Camera':0, u'BluetoothClient':0, 
+                  u'Texting':0, u'Label':0, u'Spinner':0, u'Camera':0, u'BluetoothClient':0,
                   u'PhoneCall':0, u'LocationSensor':0, u'VerticalArrangement':0,
                   u'HorizontalArrangement':0, u'Sharing':0, u'TextToSpeech':0,
-                  u'GoogleMap' :0, u'Slider':0, u'OrientationSensor':0, u'ListView':0, 
+                  u'GoogleMap' :0, u'Slider':0, u'OrientationSensor':0, u'ListView':0,
                   u'PhoneNumberPicker':0, u'TinyDB':0, u'NxtDirectCommands':0, u'Sound':0,
                   u'ListPicker':0, u'SpeechRecognizer':0, u'Button':0, u'WebViewer':0, u'BarcodeScanner':0,
                   u'NxtDrive':0, u'Camcorder':0, u'Notifier':0, u'TextBox':0, u'AccelerometerSensor':0,
                   u'Image':0, u'VideoPlayer':0, u'TinyWebDB':0, u'Player':0, u'File':0, u'YandexTranslate':0}
-    
+
     screenNames = getScreenNames(projects)
     i= 0
     while i < len(projects):
         for screenName in screenNames[i]:
             if screenName in projects[i] and 'Components' in projects[i][screenName] and 'Type and Frequency' in projects[i][screenName]['Components']:
                 types = projects[i][screenName]['Components']['Type and Frequency']
-                for entry in types: 
-                    if entry in class_dict: 
+                for entry in types:
+                    if entry in class_dict:
                         class_dict[entry] += projects[i][screenName]['Components']['Type and Frequency'][entry]
         i+=1
     final_dict = normalizeDict(class_dict, projects)
 
     return final_dict
 
-def getMathBlocks(projects): 
-    math_dict = {'math_divide':0, u'math_cos':0, u'math_format_as_decimal':0,
-                    u'math_neg':0, u'math_trig':0, u'math_floor':0, u'math_random_float':0,
-                    u'math_number':0, u'math_on_list':0, u'math_round':0, u'math_tan':0,
-                    u'math_multiply':0, u'math_atan2':0, u'math_convert_angles':0, 
-                    u'math_random_int':0, u'math_compare':0, u'math_random_set_seed':0,
-                    u'math_ceiling':0, u'math_subtract':0, u'math_single':0, u'math_power':0,
-                    u'math_abs':0, u'math_is_a_number':0, u'math_division':0, 
-                    u'math_convert_number':0, u'math_add':0, 'logic_boolean':0,
-                 'logic_compare':0,'logic_false':0,'logic_negate':0,'logic_operation':0,'logic_or':0}
-    
-    screenNames = getScreenNames(projects)
-    i= 0
-    while i < len(projects):
-        for screenName in screenNames[i]:
-                if screenName in projects[i] and 'Active Blocks' in projects[i][screenName]['Blocks'] and 'Types' in projects[i][screenName]['Blocks']['Active Blocks']:
-                    blocks = projects[i][screenName]['Blocks']['Active Blocks']['Types']
-                    for entry in blocks: 
-                        if entry in math_dict: 
-                            math_dict[entry] += projects[i][screenName]['Blocks']['Active Blocks']['Types'][entry]
-        i+=1
-    final_dict = normalizeDict(math_dict, projects)
+def getMathBlocks(projects):
+    math_set = {'math_divide', 'math_cos', 'math_format_as_decimal',
+                'math_neg', 'math_trig', 'math_floor', 'math_random_float',
+                'math_number', 'math_on_list', 'math_round', 'math_tan',
+                'math_multiply', 'math_atan2', 'math_convert_angles',
+                'math_random_int', 'math_compare', 'math_random_set_seed',
+                'math_ceiling', 'math_subtract', 'math_single', 'math_power',
+                'math_abs', 'math_is_a_number', 'math_division',
+                'math_convert_number', 'math_add',
+                'logic_boolean', 'logic_compare', 'logic_false',
+                'logic_negate', 'logic_operation','logic_or'}
 
-    
-    return final_dict
+    return getBlocks(projects, math_set)
 
 def decileDict(projects):
     '''returns a dictionary with the key as the project name and the value as the decile'''
     deciles = {}
     decile_width = len(projects)/10
     if decile_width == 0: decile_width = 1
-    for project in projects: 
+    for project in projects:
         index = projects.index(project) / decile_width
-        if index > 9: 
+        if index > 9:
             index = 9
-        
+
         deciles[project['**Project Name']] = index
-        
+
     return deciles
 
 def getAverageDecileValues(decile_list):
     '''returns the average of each decile'''
     return numpy.nan_to_num([numpy.average(sub_list) for sub_list in decile_list])
 
-def getBlocks(projects, block_dict): 
-    '''returns  a dictionary with the key as a block name ie math_add 
+def getBlocksProject(project, block_set):
+    '''returns  a dictionary with the key as a block name ie math_add
+    and a value that is the # of occurances of that block in the project'''
+    block_dict = Counter()
+    for screenName in project:
+        if '*' not in screenName and 'Active Blocks' in project[screenName]['Blocks'] and 'Types' in project[screenName]['Blocks']['Active Blocks']:
+            blocks = projects[i][screenName]['Blocks']['Active Blocks']['Types']
+            for block in blocks:
+                if block in block_set:
+                    block_dict[block] += projects[screenName]['Blocks']['Active Blocks']['Types'][block]
+    return block_dict
+
+def getBlocks(projects, block_set):
+    '''returns  a dictionary with the key as a block name ie math_add
     and a value that is the # of occurances of that block / numProjects'''
-    screenNames = getScreenNames(projects)
-    i= 0
-    while i < len(projects):
-        for screenName in screenNames[i]:
-                if screenName in projects[i] and 'Active Blocks' in projects[i][screenName]['Blocks'] and 'Types' in projects[i][screenName]['Blocks']['Active Blocks']:
-                    blocks = projects[i][screenName]['Blocks']['Active Blocks']['Types']
-                    for block in blocks: 
-                         if block in block_dict: 
-                            block_dict[block] += projects[i][screenName]['Blocks']['Active Blocks']['Types'][block]
-        i+=1
+    block_dict = Counter()
+    for project in projects:
+        block_dict += getBlocksProject(project, block_set)
     final_dict = normalizeDict(block_dict, projects)
 
     return final_dict
@@ -351,7 +344,7 @@ def decileOrphanBlocks(projects):
         all_deciles.append([])
 
 
-    i = 0 
+    i = 0
     numScreens = getScreenNames(projects)
 
     for project in projects:
@@ -371,20 +364,20 @@ def decileTypesTopLevelBlocks(projects):
     '''measures how many types of TLblocks per project in each decile '''
     numScreens = getScreenNames(projects)
     decile_dict = decileDict(projects)
-    all_deciles = [] 
-    
+    all_deciles = []
+
     for i in range(10):
         all_deciles.append([])
-    i= 0  
+    i= 0
     for project in projects:
         numTL = 0
-        
+
         for screenName in numScreens[i]:
             if screenName in project:
                 if 'Blocks' in project[screenName] and project[screenName]['Blocks'] != 'NO BLOCKS' and project[screenName]['Blocks'] != 'MALFORMED BKYFILE':
-                    try: 
+                    try:
                         numTL = len(project[screenName]['Blocks']['*Top Level Blocks'])
-                    except TypeError: 
+                    except TypeError:
                         print project[screenName]
                     index = decile_dict[project['**Project Name']]
                     all_deciles[index].append(numTL)
@@ -400,12 +393,9 @@ def decileNumScreens(projects):
 
     for i in range(10):
         all_deciles.append([])
-    
+
     for project in projects:
         numScreens = project['*Number of Screens']
         index = decile_dict[project['**Project Name']]
         all_deciles[index].append(numScreens)
     return getAverageDecileValues(all_deciles)
-                    
-
-
