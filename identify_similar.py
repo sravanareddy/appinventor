d9696cb79b61a9c82e5b1837b0d10bcde9cb0879
Sravana Reddy <sravana@cs.dartmouth.edu>
2017-05-21 15:48:38 -0400

    some ann stuff, more project name exploration

--- identify_similar.py
+++ identify_similar.py
@@ -7,6 +7,7 @@ from sklearn.feature_extraction import DictVectorizer
 import time
 import networkx as nx
 import numpy as np
+import sys
 
 def print_datastats(blockcounts):
     print len(blockcounts), 'users and', sum(map(len, blockcounts.values())), 'projects'
@@ -23,45 +24,67 @@ def user_project_filter(counts, user_project_set):
 def jaccardsim(v1, v2):
     return np.sum(np.minimum(v1, v2))/np.sum(np.maximum(v1, v2))
 
-def vectorize(binarize=False):
+def vectorize(blockcounts):
     # pre-compute block-count array for each project
     all_projects = []
     project_names = []
     for user in blockcounts:
         for project in blockcounts[user]:
-            if user in nontutorials and project in nontutorials[user]:
-                all_projects.append(blockcounts[user][project])
-                project_names.append(user+'-'+project)
+            all_projects.append(blockcounts[user][project])
+            project_names.append(user+'-'+project)
 
     vectorizer = DictVectorizer()
     project_vectors = vectorizer.fit_transform(all_projects)
-    if binarize:
-        binarizer = Binarizer()
-        project_vectors = binarizer.fit_transform(project_vectors)
     print 'Vectorized'
     print project_vectors.shape
     return project_vectors, vectorizer.vocabulary_, project_names
 
-def write_pairwise(project_vectors, project_names, outfile):
+def pairwise(project_vectors, project_names, ann):
     distances = {}
     G = nx.Graph()
-    for i, project1 in enumerate(project_names):
-        for j, project2 in enumerate(project_names[i+1:]):
-            if project1[:5]!=project2[:5]: # should be different users
-                sim = jaccardsim(project_vectors[i], project_vectors[i+j+1, :])
-                if sim>=0.5:
-                    G.add_edge(project1, project2, weight=sim)
-        if (i+1)%5==0:
-            print i+1,
+
+    if ann=='ball':
+        binarizer = Binarizer()
+        project_vectors_bin = binarizer.fit_transform(project_vectors)
+        print 'Binarized'
+
+        tree = BallTree(project_vectors_bin, metric='jaccard', leaf_size=len(project_names)/100)
+        neighbors, distances = tree.query_radius(project_vectors_bin, 0.7, return_distance=True)
+        print 'Retrieved neighbors'
+        for i, project1 in enumerate(project_names):
+            for ji, j in enumerate(neighbors[i]):
+                project2 = project_names[j]
+                if project1[:5]!=project2[:5]: # ignore if projects are from the same user
+                    G.add_edge(project1, project2, weight=1-distances[i][ji])
+
+    elif ann=='annoy':
+        from annoy import AnnoyIndex
+        tree = AnnoyIndex(project_vectors.shape[1], metric='angular')
+        for i in range(project_vectors.shape[0]):
+            tree.add_item(i, project_vectors[i])
+        tree.build(1000)
+
+        for i, project1 in enumerate(project_names):
+            neighbors, distances = tree.get_nns_by_item(i, 100, include_distances=True)
+            for ji, j in enumerate(neighbors):
+                project2 = project_names[j]
+                if project1[:5]!=project2[:5] and distances[ji]<0.5: # ignore if projects are from the same user
+                    G.add_edge(project1, project2, weight=1-distances[ji])
+            if (i+1)%5==0:
+                print i+1,
+
     print 'Computed similarities'
+    return G
+
+def write_neighbors(G, outfile):
     neighbors = {}
-    for project in project_names:
-        if G.has_node(project) and G.degree(project)>=5:
+    for project in G.nodes():
+        if G.degree(project)>=5:
             neighbors[project] = G[project]
     with open(outfile, 'w') as o:
         ujson.dump(neighbors, o)
 
-if __name__=='__main__':
+def main(ann):
     # loads pre-computed counts of each active block type per project for every user
     blockcounts = ujson.load(open('user_project_allblockcounts.json'))
     print_datastats(blockcounts)
@@ -71,6 +94,10 @@ if __name__=='__main__':
     blockcounts = user_project_filter(blockcounts, nontutorials)
     print_datastats(blockcounts)
     # vectorize
-    project_vectors, vocab, project_names = vectorize()
+    project_vectors, vocab, project_names = vectorize(blockcounts)
     # compute similarities
-    write_pairwise(project_vectors[:5000].todense(), project_names[:5000], 'all.neighbors')
+    G = pairwise(project_vectors.todense(), project_names, ann)
+    write_neighbors(G, 'all.neighbors')
+
+if __name__=='__main__':
+    main(sys.argv[1])
