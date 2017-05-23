e00eecd85fb73083cb9246f5a2e17c8f8c005e5b
Sravana Reddy <sravana@cs.dartmouth.edu>
2017-05-23 17:06:44 -0400

    decoupled data loading, decoupled tree building and inference, removed balltree

---
+++ filter_data.py
@@ -0,0 +1,59 @@
+import ujson
+import numpy as np
+from sklearn.feature_extraction import DictVectorizer
+import codecs
+from sklearn.datasets import dump_svmlight_file
+
+def print_datastats(blockcounts):
+    print len(blockcounts), 'users and', sum(map(len, blockcounts.values())), 'projects'
+
+def user_project_filter(counts, user_project_set):
+    """only keep projects in user_project_set in counts"""
+    filtered_counts = {}
+    for user in counts:
+        orig = set(user_project_set[user]) if user in user_project_set else set()
+        if len(orig)>0:   # keep users who are active after removing projects                    
+            filtered_counts[user] = {project: counts[user][project] for project in counts[user] if project in orig}
+    return filtered_counts
+
+def vectorize(blockcounts):
+    # pre-compute block-count array for each project                                          
+    all_projects = []
+    project_names = []
+    for user in blockcounts:
+        for project in blockcounts[user]:
+            all_projects.append(blockcounts[user][project])
+            project_names.append(user+'-'+project)
+
+    vectorizer = DictVectorizer()
+    project_vectors = vectorizer.fit_transform(all_projects)
+    print 'Vectorized', project_vectors.shape
+    return project_vectors, project_names, vectorizer.vocabulary_,
+
+def main():
+    # loads pre-computed counts of each active block type per project for every user        
+    blockcounts = ujson.load(open('user_project_allblockcounts.json'))
+    print_datastats(blockcounts)
+    # load user languages                                                                        
+    user_inferredlangs = ujson.load(open('user_inferredlangs.json'))
+    common_langs = set(['el', 'fr', 'en', 'zh', 'pt', 'ca', 'de', 'ko', 'it', 'th', 'es'])
+    blockcounts = {user: blockcounts[user] for user in blockcounts if user in user_inferredlangs and user_inferredlangs[user] in common_langs}
+    print_datastats(blockcounts)
+    # load non-tutorials                                                        
+    nontutorials = ujson.load(open('user_nontutorial_projects.json'))
+    # filter non-tutorials                                                      
+    blockcounts = user_project_filter(blockcounts, nontutorials)
+    print_datastats(blockcounts)
+    # vectorize                                                                 
+    project_vectors, project_names, vocab = vectorize(blockcounts)
+    # store
+    dump_svmlight_file(project_vectors, 
+                       np.zeros(project_vectors.shape[0]), 
+                       'filtered_project_vectors.svml')
+    with codecs.open('filtered_project_names.txt', 'w', 'utf8') as o:
+        o.write('\n'.join(project_names))
+    with open('filtered_featuremap.json', 'w') as o:
+        ujson.dump(vocab, o)
+
+if __name__=='__main__':
+    main()
--- identify_similar.py
+++ identify_similar.py
@@ -1,103 +1,51 @@
 from __future__ import division
 import ujson
-from scipy.spatial.distance import pdist
-from sklearn.preprocessing import Binarizer
-from sklearn.neighbors import BallTree
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.decomposition import PCA
 from collections import defaultdict
 import time
 import networkx as nx
 import numpy as np
 import sys
-
-def print_datastats(blockcounts):
-    print len(blockcounts), 'users and', sum(map(len, blockcounts.values())), 'projects'
-
-def user_project_filter(counts, user_project_set):
-    """only keep projects in user_project_set in counts"""
-    filtered_counts = {}
-    for user in counts:
-        orig = set(user_project_set[user]) if user in user_project_set else set()
-        if len(orig)>0:   # keep users who are active after removing projects
-            filtered_counts[user] = {project: counts[user][project] for project in counts[user] if project in orig}
-    return filtered_counts
+from annoy import AnnoyIndex
+from sklearn.datasets import load_svmlight_file
+import argparse
+import codecs
 
 def jaccardsim(v1, v2):
     return np.sum(np.minimum(v1, v2))/np.sum(np.maximum(v1, v2))
 
-def vectorize(blockcounts):
-    # pre-compute block-count array for each project
-    all_projects = []
-    project_names = []
-    for user in blockcounts:
-        for project in blockcounts[user]:
-            all_projects.append(blockcounts[user][project])
-            project_names.append(user+'-'+project)
-
-    vectorizer = DictVectorizer()
-    project_vectors = vectorizer.fit_transform(all_projects)
-    print 'Vectorized', project_vectors.shape
-
-    return project_vectors, np.array(project_names), vectorizer.vocabulary_,
-
-def pairwise(project_vectors, project_names, metric):
-    distances = {}
-    G = nx.Graph()
-
+def get_slices(project_vectors, project_names, sliceprop, sliceindex):
     original_numprojects = project_vectors.shape[0]
-    samples = np.random.randint(0,
-                                original_numprojects,
-                                size=(int(original_numprojects/10),)) # take random sample of projects
+    slicesize = int(original_numprojects/sliceprop)
+    samples = range(sliceindex*slicesize, (sliceindex+1)*slicesize)
     project_vectors = project_vectors[samples].todense()
     project_names = project_names[samples]
+    return project_vectors, project_names
 
-    if metric=='jaccard':
-        #binarizer = Binarizer()
-        #project_vectors = binarizer.fit_transform(project_vectors)
-        #print 'Binarized'
+def build_tree(project_vectors, project_names, numtrees, outfile):
+    start = time.time()
+    tree = AnnoyIndex(project_vectors.shape[1], metric='angular')
+    for i in range(project_vectors.shape[0]):
+        p = project_vectors[i, :].tolist()[0]
+        tree.add_item(i, p)
+    tree.build(numtrees)
+    tree.save(outfile)
+    print 'Finished in', time.time()-start, 'seconds'
 
-        start = time.time()
-        tree = BallTree(project_vectors,
-                            metric='jaccard',
-                            leaf_size=len(project_names)/100)
-        print 'Built tree'
-        neighbors, distances = tree.query_radius(project_vectors,
-                                                     0.7,
-                                                     return_distance=True)
-        print 'Retrieved neighbors'
-        for i, project1 in enumerate(project_names):
-            for ji, j in enumerate(neighbors[i]):
-                project2 = project_names[j]
-                if project1[:5]!=project2[:5]: # ignore if projects are from the same user
-                    G.add_edge(project1, project2, weight=1-distances[i][ji])
-        print 'Added edges'
-        print 'Finished in', time.time()-start, 'seconds'
+def compute_neighbors(treefile, project_vectors, project_names, k):
+    """Compute k nearest neighbors for each vector in project_vectors"""
+    start = time.time()
+    G = nx.Graph()
+    tree = AnnoyIndex(project_vectors.shape[1])
+    tree.load(treefile+'.ann')
+    for i, project1 in enumerate(project_names):
+        p = project_vectors[i, :].tolist()[0]
+        neighbors, distances = tree.get_nns_by_vector(p, k, 
+                                                      include_distances=True)
+        for ji, j in enumerate(neighbors):
+            project2 = project_names[j]
+            if project1[:5]!=project2[:5] and distances[ji]<0.3: # ignore if projects are from the same user
+                G.add_edge(project1, project2, weight=1-distances[ji])
 
-
-    elif metric=='cosine':
-        start = time.time()
-
-        from annoy import AnnoyIndex
-        tree = AnnoyIndex(project_vectors.shape[1], metric='angular')
-        for i in range(project_vectors.shape[0]):
-            p = project_vectors[i, :].tolist()[0]
-            tree.add_item(i, p)
-        tree.build(1000)
-
-        for i, project1 in enumerate(project_names):
-            neighbors, distances = tree.get_nns_by_item(i, 100, include_distances=True)
-            for ji, j in enumerate(neighbors):
-                project2 = project_names[j]
-                if project1[:5]!=project2[:5] and distances[ji]<0.3: # ignore if projects are from the same user
-                    G.add_edge(project1, project2, weight=1-distances[ji])
-
-        print 'Finished in', time.time()-start, 'seconds'
-
-
-    return G
-
-def write_neighbors(G, outfile):
     neighbors = {}
     for project1 in G.nodes():
         if G.degree(project1)>=5:
@@ -107,29 +55,40 @@ def write_neighbors(G, outfile):
     # sort
     for project1 in neighbors:
         neighbors[project1] = sorted(neighbors[project1].items(), key=lambda x:x[1], reverse=True)
-    # write
-    with open(outfile, 'w') as o:
-        ujson.dump(neighbors, o, indent=4)
 
-def main(metric):
-    # loads pre-computed counts of each active block type per project for every user
-    blockcounts = ujson.load(open('user_project_allblockcounts.json'))
-    print_datastats(blockcounts)
-    # load user languages                                                                     
-    user_inferredlangs = ujson.load(open('user_inferredlangs.json'))
-    common_langs = set(['el', 'fr', 'en', 'zh', 'pt', 'ca', 'de', 'ko', 'it', 'th', 'es']) 
-    blockcounts = {user: blockcounts[user] for user in blockcounts if user in user_inferredlangs and user_inferredlangs[user] in common_langs}
-    print_datastats(blockcounts)
-    # load non-tutorials
-    nontutorials = ujson.load(open('user_nontutorial_projects.json'))
-    # filter non-tutorials
-    blockcounts = user_project_filter(blockcounts, nontutorials)
-    print_datastats(blockcounts)
-    # vectorize
-    project_vectors, project_names, vocab = vectorize(blockcounts)
-    # compute similarities between projects within the same language
-    G = pairwise(project_vectors, project_names, metric)
-    write_neighbors(G, 'all2.neighbors')
-
+    print 'Finished in', time.time()-start, 'seconds'
+    
+    return neighbors
+    
 if __name__=='__main__':
-    main(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--build', help='build tree', action="store_true")
+    parser.add_argument('sliceprop', help='slice size as a proportion of the data', type=int)
+    parser.add_argument('--numtrees', type=int)
+    parser.add_argument('--k', help='number of nearest neighbors', type=int)
+    parser.add_argument('--treefile', help='filename with stored tree')
+    args = parser.parse_args()
+
+    project_vectors, _ = load_svmlight_file('filtered_project_vectors.svml', dtype=np.int16)
+    project_names = np.array(codecs.open('filtered_project_names.txt', 'r', 'utf8').read().split())
+
+    if args.build:
+        # build trees from slices                                                                 
+        for sliceindex in range(args.sliceprop):
+            project_vectors, project_names = get_slices(project_vectors,
+                                                    project_names,
+                                                    args.sliceprop,
+                                                    sliceindex)
+            outfile = 'tree{0}-{1}-{2}.ann'.format(args.numtrees, args.sliceprop, sliceindex)
+            build_tree(project_vectors, project_names, args.numtrees, outfile)
+    else:
+        for sliceindex in range(args.sliceprop):
+            project_vectors, project_names = get_slices(project_vectors,
+                                                        project_names,
+                                                        args.sliceprop,
+                                                        sliceindex)
+            neighbors = compute_neighbors(args.treefile, project_vectors, project_names, args.k)
+            outfile = 'neighbors-{0}-{1}-{2}.json'.format(args.treefile, sliceprop, sliceindex)
+            with open(outfile, 'w') as o:
+                ujson.dump(neighbors, o, indent=3)
+            print sliceindex
