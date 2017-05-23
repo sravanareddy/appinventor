e801cfcabee7a3a5a9b1d17dd3ba1e3ea4336ac0
Sravana Reddy <sravana@cs.dartmouth.edu>
2017-05-22 23:38:42 -0400

    language filtering comes first

--- identify_similar.py
+++ identify_similar.py
@@ -19,23 +19,21 @@ def user_project_filter(counts, user_project_set):
     filtered_counts = {}
     for user in counts:
         orig = set(user_project_set[user]) if user in user_project_set else set()
-        if len(orig)>=10:   # keep users who are active after removing projects
+        if len(orig)>0:   # keep users who are active after removing projects
             filtered_counts[user] = {project: counts[user][project] for project in counts[user] if project in orig}
     return filtered_counts
 
 def jaccardsim(v1, v2):
     return np.sum(np.minimum(v1, v2))/np.sum(np.maximum(v1, v2))
 
-def vectorize(blockcounts, user_inferredlangs):
+def vectorize(blockcounts):
     # pre-compute block-count array for each project
     all_projects = []
     project_names = []
-    for user in user_inferredlangs:
-        lang = user_inferredlangs[user]
-        if user in blockcounts and lang in ['el', 'fr', 'en', 'zh', 'pt', 'ca', 'de', 'ko', 'it', 'th', 'es']:
-            for project in blockcounts[user]:
-                all_projects.append(blockcounts[user][project])
-                project_names.append(user+'-'+project)
+    for user in blockcounts:
+        for project in blockcounts[user]:
+            all_projects.append(blockcounts[user][project])
+            project_names.append(user+'-'+project)
 
     vectorizer = DictVectorizer()
     project_vectors = vectorizer.fit_transform(all_projects)
@@ -91,7 +89,7 @@ def pairwise(project_vectors, project_names, metric):
             neighbors, distances = tree.get_nns_by_item(i, 100, include_distances=True)
             for ji, j in enumerate(neighbors):
                 project2 = project_names[j]
-                if project1[:5]!=project2[:5] and distances[ji]<0.7: # ignore if projects are from the same user
+                if project1[:5]!=project2[:5] and distances[ji]<0.3: # ignore if projects are from the same user
                     G.add_edge(project1, project2, weight=1-distances[ji])
 
         print 'Finished in', time.time()-start, 'seconds'
@@ -117,18 +115,21 @@ def main(metric):
     # loads pre-computed counts of each active block type per project for every user
     blockcounts = ujson.load(open('user_project_allblockcounts.json'))
     print_datastats(blockcounts)
+    # load user languages                                                                     
+    user_inferredlangs = ujson.load(open('user_inferredlangs.json'))
+    common_langs = set(['el', 'fr', 'en', 'zh', 'pt', 'ca', 'de', 'ko', 'it', 'th', 'es']) 
+    blockcounts = {user: blockcounts[user] for user in blockcounts if user in user_inferredlangs and user_inferredlangs[user] in common_langs}
+    print_datastats(blockcounts)
     # load non-tutorials
     nontutorials = ujson.load(open('user_nontutorial_projects.json'))
     # filter non-tutorials
     blockcounts = user_project_filter(blockcounts, nontutorials)
     print_datastats(blockcounts)
-    # load user languages
-    user_inferredlangs = ujson.load(open('user_inferredlangs.json'))
     # vectorize
-    project_vectors, project_names, vocab = vectorize(blockcounts, user_inferredlangs)
+    project_vectors, project_names, vocab = vectorize(blockcounts)
     # compute similarities between projects within the same language
     G = pairwise(project_vectors, project_names, metric)
-    write_neighbors(G, 'all.neighbors')
+    write_neighbors(G, 'all2.neighbors')
 
 if __name__=='__main__':
     main(sys.argv[1])
