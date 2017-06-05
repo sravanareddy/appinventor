3f23f18bcf427f91f63c9b7ffb3a9fe0ef88817b
Your Name <sravana@spotify.com>
2017-06-05 17:08:44 -0400

    no folds, fixed split, ignore english

--- plotcnf.py
+++ plotcnf.py
@@ -6,7 +6,7 @@ import itertools
 """Code from http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py to visualize confusion matrix"""
 
 
-def plot_confusion_matrix(cm, classes, normed=False, cmap=plt.cm.Blues):
+def plot_confusion_matrix(cm, classes, showtext = True, normed=False, cmap=plt.cm.Blues):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
@@ -15,22 +15,22 @@ def plot_confusion_matrix(cm, classes, normed=False, cmap=plt.cm.Blues):
         cm = normalize(cm, norm='l1')
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title('Confusion Matrix')
     plt.colorbar()
     tick_marks = range(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
 
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, '{:.2f}'.format(cm[i, j]),
+    if showtext:
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, '{:.2f}'.format(cm[i, j]),
                  horizontalalignment="center",
                  color="white" if cm[i, j] > thresh else "black")
 
-    if normed:
-        plt.title("Normalized confusion matrix")
-    else:
-        plt.title('Confusion matrix, without normalization')
+    #if normed:
+    #    plt.title("Normalized confusion matrix")
+    #else:
+    #    plt.title('Confusion matrix, without normalization')
 
     plt.tight_layout()
     plt.ylabel('True label')
